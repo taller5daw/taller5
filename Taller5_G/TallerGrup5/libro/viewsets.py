c2cf78a6a126c8131f3b0f7c061f428ab2f675b8
Xavier Figueroa <xavierfigueroav@gmail.com>
2019-08-26 20:13:48 -0500

    Add single-book page. Add rating capability.
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- Taller5_G/TallerGrup5/libro/viewsets.py
+++ Taller5_G/TallerGrup5/libro/viewsets.py
@@ -12,15 +12,19 @@ class AutorViewSet(viewsets.ModelViewSet):
 	queryset = Autor.objects.all()
 	serializer_class = AutorSerializer
 
+	def get_queryset(self):
+		queryset = Autor.objects.all()
+		libro = self.request.query_params.get('libro', None)
+		if libro is not None:
+			autores = map(lambda pair: pair.id_autor.id_autor, Autor_Libro.objects.filter(id_libro=libro))
+			queryset = Autor.objects.filter(pk__in=autores)
+		return queryset
+
 class LibroViewSet(viewsets.ModelViewSet):
 	queryset = Libro.objects.all()
 	serializer_class = LibroSerializer
 
 	def get_queryset(self):
-		"""
-		Optionally restricts the returned purchases to a given user,
-		by filtering against a `username` query parameter in the URL.
-		"""
 		queryset = Libro.objects.all()
 		autor = self.request.query_params.get('autor', None)
 		if autor is not None:
@@ -35,6 +39,14 @@ class PersonaViewSet(viewsets.ModelViewSet):
 class RateViewSet(viewsets.ModelViewSet):
 	queryset = Rate.objects.all()
 	serializer_class = RateSerializer
+
+	def get_queryset(self):
+		queryset = Rate.objects.all()
+		user = self.request.query_params.get('user', None)
+		libro = self.request.query_params.get('libro', None)
+		if user is not None and libro is not None:
+			queryset = Rate.objects.filter(user=user, id_libro=libro)
+		return queryset
 
 class AutorLibroViewSet(viewsets.ModelViewSet):
 	queryset = Autor_Libro.objects.all()
